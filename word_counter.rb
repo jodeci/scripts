fa443178c4370db35a2a2c3d0e4e47ceed49c3f8
Tsehau Chao <jodeci@5xruby.com>
2025-07-01 11:01:29 +0800

    Handle curly apostrophes and update stop words

--- word_counter.rb
+++ word_counter.rb
@@ -6,6 +6,10 @@ abort "usage: #{__FILE__} [markdown_file]" if ARGV.empty?
 file = ARGV[0]
 text = File.read(file)
 
+# Normalize curly apostrophes to regular ones so words like “couldn’t”
+# are tokenized correctly
+text.gsub!(/[\u2018\u2019]/, "'")
+
 # Remove dialogue inside double quotes
 text.gsub!(/"[^"]*"/, '')
 
@@ -13,7 +17,7 @@ text.gsub!(/"[^"]*"/, '')
 words = text.downcase.scan(/[a-z']+/)
 
 # Stop words to place at the end
-STOP_WORDS = %w[a an the he she it his her him their they them is am are was were be been being have has had do does did to in on at for and or but if then with by of from].freeze
+STOP_WORDS = %w[a an the he she it his her him their they them is am are was were be been being have has had do does did to in on at for and or but if then with by of from this i as that].freeze
 
 # Simple stemming to group different tenses
 # This is a naive approach; for full stemming use a dedicated library
